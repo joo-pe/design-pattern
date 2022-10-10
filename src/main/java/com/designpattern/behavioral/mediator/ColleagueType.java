0b2c2ffb88fd66130f92be6dfd1f23e75328d956
jaden <joohyung1002@naver.com>
2022-10-10 11:38:10 +0900

    mediator pattern

---
+++ src/main/java/com/designpattern/behavioral/mediator/ColleagueType.java
@@ -0,0 +1,5 @@
+package com.designpattern.behavioral.mediator;
+
+public enum ColleagueType {
+    USER, SYSTEM, ADMIN
+}
---
+++ src/main/java/com/designpattern/behavioral/mediator/Mediator.java
@@ -0,0 +1,8 @@
+package com.designpattern.behavioral.mediator;
+
+public interface Mediator {
+
+    void addColleague(Colleague colleague);
+
+    void mediate(Colleague colleague);
+}
